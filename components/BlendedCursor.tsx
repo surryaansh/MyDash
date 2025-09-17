3d230590b24cb7e8f4d654645bf5807f4192297f
surryaansh <suryanshs1804@gmail.com>
2025-09-17 15:55:49 +0530

    d

--- components/BlendedCursor.tsx
+++ components/BlendedCursor.tsx
@@ -29,24 +29,32 @@ export const BlendedCursor: React.FC<BlendedCursorProps> = ({ position, isHoveri
     return null;
   }
 
+  const borderColor = isDarkMode ? '#efeeee' : 'black';
+
+  const cursorStyle: React.CSSProperties = {
+    position: 'fixed',
+    top: position.y,
+    left: position.x,
+    width: `${isHoveringLink ? 60 : 40}px`,
+    height: `${isHoveringLink ? 60 : 40}px`,
+    borderRadius: '50%',
+    pointerEvents: 'none',
+    transform: 'translate(-50%, -50%)',
+    zIndex: 9999,
+    transition: 'width 0.2s ease, height 0.2s ease, opacity 0.5s ease-in-out, background-color 0.2s ease, border-color 0.2s ease',
+    opacity: applyCursorFadeIn ? 1 : 0,
+    boxSizing: 'border-box', // Ensure border doesn't add to the element's size
+    
+    // Conditional styles for hovering orange elements
+    mixBlendMode: isHoveringOrange ? 'normal' : 'difference',
+    backgroundColor: isHoveringOrange ? 'transparent' : 'white',
+    border: `2px solid ${isHoveringOrange ? borderColor : 'transparent'}`,
+  };
+
   return (
     <div className="hidden lg:block">
       <div
-        style={{
-          position: 'fixed',
-          top: position.y,
-          left: position.x,
-          width: `${isHoveringLink ? 60 : 40}px`,
-          height: `${isHoveringLink ? 60 : 40}px`,
-          borderRadius: '50%',
-          pointerEvents: 'none',
-          transform: 'translate(-50%, -50%)',
-          zIndex: 9999,
-          transition: 'width 0.2s ease, height 0.2s ease, opacity 0.5s ease-in-out, background-color 0.2s ease',
-          opacity: applyCursorFadeIn ? 1 : 0,
-          backgroundColor: isHoveringOrange ? (isDarkMode ? '#efeeee' : 'black') : 'white',
-          mixBlendMode: isHoveringOrange ? 'normal' : 'difference',
-        }}
+        style={cursorStyle}
         aria-hidden="true"
       />
     </div>
