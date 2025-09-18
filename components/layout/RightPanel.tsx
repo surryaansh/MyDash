375eaf89fd47dbb55cce27bc267cb18e76fb6b09
surryaansh <suryanshs1804@gmail.com>
2025-09-18 23:29:07 +0530

    Update RightPanel.tsx

--- components/layout/RightPanel.tsx
+++ components/layout/RightPanel.tsx
@@ -23,7 +23,7 @@ export const RightPanel = forwardRef<HTMLDivElement, RightPanelProps>(
         </div>
         <div 
           ref={ref}
-          className="relative flex-1 aspect-square lg:aspect-auto overflow-hidden px-2 pt-0 pb-8 lg:px-0"
+          className="relative flex-1 overflow-hidden px-2 pt-0 pb-12 lg:px-0"
         >
           {/* This is the solid cursor, only visible when inside this container */}
           <div
@@ -46,10 +46,10 @@ export const RightPanel = forwardRef<HTMLDivElement, RightPanelProps>(
           <img 
               src="/vaporwave-david.png"
               alt="Vaporwave style statue of David wearing a glowing crown and glasses."
-              className="absolute inset-0 w-full h-full object-cover"
+              className="w-full h-full object-cover"
           />
           <FilledLightningIcon 
-            className="absolute bottom-8 right-6 md:bottom-16 md:right-[5%] text-[#FF4500] w-28 h-28 md:w-[9.409rem] md:h-[9.409rem]"
+            className="absolute bottom-12 right-6 md:bottom-20 md:right-[5%] text-[#FF4500] w-28 h-28 md:w-[9.409rem] md:h-[9.409rem]"
             style={{ mixBlendMode: 'normal' }}
           />
         </div>
