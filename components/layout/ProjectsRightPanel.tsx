47589e28bc03688b0ec182fd7f8f24c34667ed90
surryaansh <suryanshs1804@gmail.com>
2025-09-21 22:30:33 +0530

    Update ProjectsRightPanel.tsx

--- components/layout/ProjectsRightPanel.tsx
+++ components/layout/ProjectsRightPanel.tsx
@@ -25,8 +25,8 @@ export const ProjectsRightPanel: React.FC<ProjectsRightPanelProps> = ({ isDarkMo
     );
   }
 
-  // Updated classes: removed rounded corners, added a thin black border.
-  const imageBaseClasses = `absolute object-cover border border-black shadow-lg transition-all duration-300 hover:scale-105 hover:z-40`;
+  // Updated classes: removed shadow, increased hover z-index.
+  const imageBaseClasses = `absolute object-cover border border-black transition-all duration-300 hover:scale-105 hover:z-50`;
 
   return (
     <div className="w-full lg:col-span-2 flex flex-col lg:pl-6 pt-8 lg:pt-0">
@@ -38,41 +38,38 @@ export const ProjectsRightPanel: React.FC<ProjectsRightPanelProps> = ({ isDarkMo
         <div className="w-full md:w-1/3 flex flex-col">
           <p className="text-base leading-relaxed">{project.description}</p>
         </div>
-        {/* New Organized & Balanced Collage Layout */}
-        <div className="w-full md:w-2/3 min-h-[400px] md:min-h-[500px] relative">
-          {/* Image 1: Main background image, top-left */}
+        
+        {/* New "One-by-One" Linear Collage Layout */}
+        <div className="w-full md:w-2/3 min-h-[500px] md:min-h-[600px] relative">
+          {/* Image 1: Left-most */}
           <img
             src={project.images[0]}
             alt={`${project.name} screenshot 1`}
-            className={`${imageBaseClasses} w-2/3 h-auto top-0 left-0`}
-            style={{ zIndex: 10 }}
+            className={`${imageBaseClasses} top-[10%] left-0 w-2/5 h-auto z-10`}
             aria-hidden="true"
           />
 
-          {/* Image 2: Tall portrait image, right side */}
+          {/* Image 2: Middle-left */}
           <img
             src={project.images[1]}
             alt={`${project.name} screenshot 2`}
-            className={`${imageBaseClasses} w-2/5 h-auto top-[10%] right-0`}
-            style={{ zIndex: 20, maxHeight: '80%' }}
+            className={`${imageBaseClasses} top-[25%] left-[20%] w-2/5 h-auto z-20`}
             aria-hidden="true"
           />
 
-          {/* Image 4: The 'too big' one, now smaller and at the bottom */}
-          <img
-            src={project.images[3]}
-            alt={`${project.name} screenshot 4`}
-            className={`${imageBaseClasses} w-1/2 h-auto bottom-0 left-[5%]`}
-            style={{ zIndex: 30 }}
-            aria-hidden="true"
-          />
-
-          {/* Image 3: Small accent image, overlapping with a stronger shadow */}
+          {/* Image 3: Middle-right */}
           <img
             src={project.images[2]}
             alt={`${project.name} screenshot 3`}
-            className={`${imageBaseClasses} w-1/3 h-auto bottom-[20%] right-[10%] shadow-2xl`}
-            style={{ zIndex: 40 }}
+            className={`${imageBaseClasses} top-[5%] left-[40%] w-2/5 h-auto z-30`}
+            aria-hidden="true"
+          />
+
+          {/* Image 4: Right-most (the tall one) */}
+          <img
+            src={project.images[3]}
+            alt={`${project.name} screenshot 4`}
+            className={`${imageBaseClasses} top-[15%] left-[60%] w-2/5 h-auto max-h-[80%] z-40`}
             aria-hidden="true"
           />
         </div>
