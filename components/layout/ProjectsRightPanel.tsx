fcdc8cc5d795a3b6fc43a368cbabe4c2378a4f0c
surryaansh <suryanshs1804@gmail.com>
2025-09-21 23:01:58 +0530

    Update ProjectsRightPanel.tsx

--- components/layout/ProjectsRightPanel.tsx
+++ components/layout/ProjectsRightPanel.tsx
@@ -34,49 +34,49 @@ export const ProjectsRightPanel: React.FC<ProjectsRightPanelProps> = ({ isDarkMo
         <span>03 WORK</span>
         <span>/03</span>
       </div>
-      <div key={project.name} className="flex-1 flex flex-col md:flex-row gap-8 py-8 animate-fade-in">
-        <div className="w-full md:w-1/2 flex flex-col">
-          <p className="text-base leading-relaxed">{project.description}</p>
-        </div>
+      
+      {/* 
+        The key change is here: The images are now in a container that takes up the full space,
+        not sharing it with a description. This allows for positioning based on the user's sketch.
+      */}
+      <div key={project.name} className="flex-1 relative w-full py-8 animate-fade-in" style={{ minHeight: '400px' }}>
         
-        {/* New Collage Layout based on user's sketch */}
-        <div className="w-full md:w-1/2 relative mt-8 md:mt-0" style={{ minHeight: '450px' }}>
-          {/* Image 1 (Bottom Left, Horizontal) */}
-          <img
-            src={project.images[0]}
-            alt={`${project.name} screenshot 1`}
-            className={`${imageBaseClasses} w-3/5 h-auto bottom-0 left-0`}
-            style={{ zIndex: 10 }}
-            aria-hidden="true"
-          />
+        {/* Image 1 (Bottom Left, Horizontal) */}
+        <img
+          src={project.images[0]}
+          alt={`${project.name} screenshot 1`}
+          className={`${imageBaseClasses} w-3/5 h-auto bottom-0 left-0`}
+          style={{ zIndex: 10 }}
+          aria-hidden="true"
+        />
 
-          {/* Image 2 (Top Center, Vertical) */}
-          <img
-            src={project.images[1]}
-            alt={`${project.name} screenshot 2`}
-            className={`${imageBaseClasses} w-2/5 h-auto top-[10%] left-[20%]`}
-            style={{ zIndex: 20 }}
-            aria-hidden="true"
-          />
+        {/* Image 2 (Top Center, Vertical) */}
+        <img
+          src={project.images[1]}
+          alt={`${project.name} screenshot 2`}
+          className={`${imageBaseClasses} w-2/5 h-auto top-[10%] left-[20%]`}
+          style={{ zIndex: 20 }}
+          aria-hidden="true"
+        />
 
-          {/* Image 4 (Top Right, Tall Vertical) */}
-          <img
-            src={project.images[3]}
-            alt={`${project.name} screenshot 4`}
-            className={`${imageBaseClasses} w-1/3 h-5/6 top-0 right-0 object-top`}
-            style={{ zIndex: 30 }}
-            aria-hidden="true"
-          />
+        {/* Image 4 (Top Right, Tall Vertical) */}
+        <img
+          src={project.images[3]}
+          alt={`${project.name} screenshot 4`}
+          className={`${imageBaseClasses} w-1/3 h-5/6 top-0 right-0 object-top`}
+          style={{ zIndex: 30 }}
+          aria-hidden="true"
+        />
 
-          {/* Image 3 (Middle Right, Horizontal) */}
-          <img
-            src={project.images[2]}
-            alt={`${project.name} screenshot 3`}
-            className={`${imageBaseClasses} w-1/2 h-auto top-1/3 left-1/2 -translate-x-1/4`}
-            style={{ zIndex: 40 }}
-            aria-hidden="true"
-          />
-        </div>
+        {/* Image 3 (Middle Right, Horizontal) */}
+        <img
+          src={project.images[2]}
+          alt={`${project.name} screenshot 3`}
+          className={`${imageBaseClasses} w-1/2 h-auto top-1/3 left-1/2 -translate-x-1/4`}
+          style={{ zIndex: 40 }}
+          aria-hidden="true"
+        />
+
       </div>
     </div>
   );
