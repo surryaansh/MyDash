b45006d67e5e68a417f37fc587ba56a1c997e1ab
surryaansh <suryanshs1804@gmail.com>
2025-09-21 22:47:01 +0530

    Update ProjectsRightPanel.tsx

--- components/layout/ProjectsRightPanel.tsx
+++ components/layout/ProjectsRightPanel.tsx
@@ -25,6 +25,8 @@ export const ProjectsRightPanel: React.FC<ProjectsRightPanelProps> = ({ isDarkMo
     );
   }
 
+  const borderClass = isDarkMode ? 'border-[#efeeee]' : 'border-black';
+
   return (
     <div className="w-full lg:col-span-2 flex flex-col lg:pl-6 pt-8 lg:pt-0">
       <div className={`flex justify-between text-[10px] py-2 ${grayTextClasses}`}>
@@ -32,17 +34,17 @@ export const ProjectsRightPanel: React.FC<ProjectsRightPanelProps> = ({ isDarkMo
         <span>/03</span>
       </div>
       <div key={project.name} className="flex-1 flex flex-col pt-8 pb-4 animate-fade-in">
-        {/* Project Description */}
-        <div className="w-full md:w-2/3 mb-12">
+        {/* Project Description (more compact) */}
+        <div className="w-full md:w-1/2 mb-12">
           <p className="text-base leading-relaxed">{project.description}</p>
         </div>
         
-        {/* Single Centered Image Layout */}
-        <div className="flex-1 flex justify-center items-start">
+        {/* Single Left-Aligned and Resized Image Layout */}
+        <div className="flex-1 flex justify-start items-start">
           <img
             src={project.images[0]}
             alt={`${project.name} screenshot 1`}
-            className="object-contain border border-black max-w-full md:max-w-xl"
+            className={`object-contain border ${borderClass} max-w-full md:max-w-lg`}
             aria-hidden="true"
           />
         </div>
