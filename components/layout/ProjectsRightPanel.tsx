d6ba6030e14fca46a9864a54c0fe99bd781a27e4
surryaansh <suryanshs1804@gmail.com>
2026-01-19 21:22:56 +0530

    Added Image Animation

--- components/layout/ProjectsRightPanel.tsx
+++ components/layout/ProjectsRightPanel.tsx
@@ -8,7 +8,9 @@ interface ProjectsRightPanelProps {
 
 export const ProjectsRightPanel: React.FC<ProjectsRightPanelProps> = ({ isDarkMode, selectedProject }) => {
   const grayTextClasses = `transition-colors duration-300 ease-in-out ${isDarkMode ? 'text-gray-400' : 'text-gray-600'}`;
-  const imageBaseClasses = `absolute object-cover border-[0.5px] border-black transition-transform duration-300 hover:scale-[1.02]`;
+  
+  // Base classes for the images including the transition properties
+  const imageBaseClasses = `absolute object-cover border-[0.5px] border-black transition-all duration-700 ease-out hover:scale-[1.02]`;
 
   return (
     <div className="w-full lg:col-span-2 flex flex-col lg:pl-6 pt-8 lg:pt-0">
@@ -22,44 +24,46 @@ export const ProjectsRightPanel: React.FC<ProjectsRightPanelProps> = ({ isDarkMo
           const isSelected = project.name === selectedProject;
           const { layout } = project;
 
+          // Animation classes based on selection state
+          const animClasses = (delayClass: string) => 
+            `${isSelected ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12 pointer-events-none'} ${delayClass}`;
+
           return (
             <div 
               key={project.name} 
-              className={`absolute inset-0 w-full h-full transition-opacity duration-300 ease-in-out ${
-                isSelected ? 'opacity-100 pointer-events-auto z-10' : 'opacity-0 pointer-events-none z-0'
-              }`}
+              className={`absolute inset-0 w-full h-full ${isSelected ? 'z-10' : 'z-0'}`}
             >
-              {/* Image 1 */}
+              {/* Image 1 - Stagger 0ms */}
               <img
                 src={project.images[0]}
                 alt={`${project.name} preview 1`}
-                className={`${imageBaseClasses} ${layout.img1}`}
+                className={`${imageBaseClasses} ${layout.img1} ${animClasses('delay-[0ms]')}`}
                 aria-hidden="true"
               />
 
-              {/* Image 2 */}
+              {/* Image 2 - Stagger 100ms */}
               <img
                 src={project.images[1]}
                 alt={`${project.name} preview 2`}
-                className={`${imageBaseClasses} ${layout.img2}`}
+                className={`${imageBaseClasses} ${layout.img2} ${animClasses('delay-[100ms]')}`}
                 aria-hidden="true"
               />
 
-              {/* Image 3 - Render only if it exists and a layout is defined */}
+              {/* Image 3 - Stagger 200ms */}
               {project.images.length > 3 && layout.img3 && (
                 <img
                   src={project.images[2]}
                   alt={`${project.name} preview 3`}
-                  className={`${imageBaseClasses} ${layout.img3}`}
+                  className={`${imageBaseClasses} ${layout.img3} ${animClasses('delay-[200ms]')}`}
                   aria-hidden="true"
                 />
               )}
 
-              {/* Highlight Image */}
+              {/* Highlight Image - Stagger 300ms */}
               <img
                 src={project.images.length > 3 ? project.images[3] : project.images[2]}
                 alt={`${project.name} main showcase`}
-                className={`${imageBaseClasses} ${layout.img4}`}
+                className={`${imageBaseClasses} ${layout.img4} ${animClasses('delay-[300ms]')}`}
                 aria-hidden="true"
               />
             </div>
