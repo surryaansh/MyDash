3f5d32a4726edd3a3142d652827dc414c52e20d8
Suryansh <suryanshs1804@gmail.com>
2026-01-20 16:08:01 +0530

    Update projects.ts

--- constants/projects.ts
+++ constants/projects.ts
@@ -31,8 +31,8 @@ export const PROJECTS_DATA = [
       '/mydash-4.png',
     ],
     layout: {
-      img1: "w-[63%] h-auto max-h-[63%] bottom-4 left-[13%] z-[10]",
-      img2: "", 
+      img1: "",
+      img2: "w-[22.5%] h-auto bottom-[12%] right-[28%] z-[30]", 
       img3: "", 
       img4: "h-[92%] w-auto bottom-4 right-0 z-[25]"
     },
