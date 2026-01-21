23894976ae8367c8e3210775b330cc5f3509169b
surryaansh <suryanshs1804@gmail.com>
2026-01-21 20:35:02 +0530

    Update project description

--- constants/projects.ts
+++ constants/projects.ts
@@ -17,7 +17,7 @@ export const PROJECTS_DATA = [
     layout: {
       img1: "w-[45%] h-auto max-h-[45%] bottom-4 left-[13%] z-[10]",
       img2: "w-[25%] h-auto top-[5%] right-[44%] z-[20]",
-      img3: "w-[22.5%] h-auto bottom-[18%] right-[17%] z-[30]",
+      img3: "w-[22.5%] h-auto bottom-[18%] right-[21%] z-[30]",
       img4: "h-[92%] w-auto bottom-4 right-0 z-[25]"
     },
     link: "#",
@@ -25,7 +25,7 @@ export const PROJECTS_DATA = [
   },
   {
     name: "MYDASH",
-    description: "This is my modern, responsive, and optimized Next.js portfolio built with TypeScript and Tailwind CSS. It leverages reusable component abstractions and serverless API routes for dynamic content updates. Deployed on Vercel with a focus on maintainability, performance, and clean design.",
+    description: "This is my modern, responsive, and optimized Next.js portfolio built with TypeScript and Tailwind CSS. It leverages reusable component abstractions and serverless API routes for dynamic content updates. Deployed on Vercel with a focus on maintainability and performance.",
     keywords: ["PRODUCTIVITY", "FRONTEND DEV", "DATA VISUALIZATION"],
     images: [
       '/mydash-2.png',
@@ -42,7 +42,7 @@ export const PROJECTS_DATA = [
   },
   {
     name: "SURU GPT",
-    description: "SuruGPT is a context-aware LLM system with RAG-based memory pipeline. Implementing persistent conversational memory using embeddings and semantic retrieval. It features automated summarization, vector storage, and relevance-based context injection at inference time.",
+    description: "SuruGPT is a context-aware LLM system with RAG-based memory pipeline. Implementing persistent conversational memory using embeddings and semantic retrieval. It features automated summarization, vector storage, and relevance-based context injection.",
     keywords: ["ARTIFICIAL INTELLIGENCE", "DEVELOPER TOOLS", "LLM"],
     images: [
       '/surugpt-1.png',
@@ -53,7 +53,7 @@ export const PROJECTS_DATA = [
     layout: {
       img1: "w-[45%] h-auto max-h-[45%] bottom-4 left-[13%] z-[10]",
       img2: "w-[25%] h-auto top-[5%] right-[44%] z-[20]",
-      img3: "w-[22.5%] h-auto bottom-[12%] right-[23%] z-[30]",
+      img3: "w-[22.5%] h-auto bottom-[12%] right-[25%] z-[30]",
       img4: "h-[92%] w-auto bottom-4 right-0 z-[25]"
     },
     link: "#",
